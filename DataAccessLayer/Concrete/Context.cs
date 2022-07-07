dc7eeaddaf1b83c83f8da7a4d05772ba7db3d974
Recep Miraç  Güneş <miracgns9@gmail.com>
2022-07-07 23:42:37 +0300

    DERS-13

--- DataAccessLayer/Concrete/Context.cs
+++ DataAccessLayer/Concrete/Context.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,11 +10,15 @@ namespace DataAccessLayer.Concrete
 {
     public class Context : DbContext
     {
-
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("server=DESKTOP-RV4J3FE;database=CoreBlogDb; integrated security=true");
+            optionsBuilder.UseSqlServer("server=DESKTOP-RV4J3FE;database=CoreBlogEnDb; integrated security=true");
         }
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Comment>  Comments  { get; set; }
+        public DbSet<Contact>  Contacts { get; set; }
+        public DbSet<Writer> Writers  { get; set; }
     }
-
 }
