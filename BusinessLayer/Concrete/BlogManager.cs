7b8f378f582f5887e69a522f2bc415635e33d1e1
Recep Miraç  Güneş <miracgns9@gmail.com>
2022-07-26 14:24:26 +0300

    Ders-66''MY-ENGLISHBLOG

--- BusinessLayer/Concrete/BlogManager.cs
+++ BusinessLayer/Concrete/BlogManager.cs
@@ -22,10 +22,15 @@ namespace BusinessLayer.Concrete
         {
             return _blogDal.GetListWithCategory();
         }
-
-        public Blog GetById(int id)
+         
+        public List<Blog> GetListWithCategoryByWriterBm(int id)
         {
-            throw new NotImplementedException();
+            return _blogDal.GetListWithCategoryByWriter(id);
+        }
+
+        public Blog TGetById(int id)
+        {
+            return _blogDal.GetByID(id);
         }
 
         public List<Blog> GetBlogByID(int id)
@@ -49,22 +54,18 @@ namespace BusinessLayer.Concrete
 
         public void TAdd(Blog t)
         {
-            throw new NotImplementedException();
+            _blogDal.Insert(t);
         }
 
         public void TDelete(Blog t)
         {
-            throw new NotImplementedException();
-        }
+            _blogDal.Delete(t);
 
+        }
+        
         public void TUpdate(Blog t)
         {
-            throw new NotImplementedException();
-        }
-
-        public Blog TGetById(int id)
-        {
-            throw new NotImplementedException();
-        }
+            _blogDal.Update(t);
+        }    
     }
 }
