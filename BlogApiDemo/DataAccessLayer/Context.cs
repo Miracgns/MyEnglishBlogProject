6730975cecef81c335c81508104a8ffb7497c274
Recep Miraç  Güneş <miracgns9@gmail.com>
2022-07-30 13:58:47 +0300

    EnglishBlogProject-API-POASTMAN//120''

---
+++ BlogApiDemo/DataAccessLayer/Context.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BlogApiDemo.DataAccessLayer
+{
+    public class Context:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=DESKTOP-RV4J3FE;database=CoreBlogEnApiDb; integrated security=true;");
+        }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ MyProjectEN/Areas/Admin/Controllers/ChartController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyProjectEN.Areas.Admin.Models;
+using System.Collections.Generic;
+
+namespace MyProjectEN.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ChartController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CategoryChart()
+        {
+            List<CategoryClass> list = new List<CategoryClass>();
+
+            list.Add(new CategoryClass
+            {
+                categoryname = "Gold",
+                categorycount = 12
+            });
+            list.Add(new CategoryClass
+            {
+                categoryname = "Silver",
+                categorycount = 8
+            });
+            list.Add(new CategoryClass
+            {
+                categoryname = "Nasdaq",
+                categorycount = 5
+            });
+            list.Add(new CategoryClass
+            {
+                categoryname = "Crypto",
+                categorycount = 13
+            });
+            return Json(new { jsonlist = list });
+        }
+    }
+}
